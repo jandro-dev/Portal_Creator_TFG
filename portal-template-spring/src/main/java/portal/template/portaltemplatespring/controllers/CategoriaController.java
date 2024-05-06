a79fbd43036f4c85195cc860bbed88e143387720
afrancop05 <jandro.develop@gmail.com>
2024-05-06 18:36:47 +0200

    Update: Controllers ( faltan metodos update y delete )

--- portal-template-spring/src/main/java/portal/template/portaltemplatespring/controllers/CategoriaController.java
+++ portal-template-spring/src/main/java/portal/template/portaltemplatespring/controllers/CategoriaController.java
@@ -1,5 +1,35 @@
 package portal.template.portaltemplatespring.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import portal.template.portaltemplatespring.models.dao.CategoriaRepository;
+import portal.template.portaltemplatespring.models.entity.Categoria;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
 public class CategoriaController {
     
+    private CategoriaRepository categoriaRepository;
+
+    public CategoriaController(CategoriaRepository _categoriaRepository) {
+        this.categoriaRepository = _categoriaRepository;
+    }
+
+    @GetMapping("/category")
+    public List<Categoria> getCategorias() {
+        return (List<Categoria>) categoriaRepository.findAll();
+    }
+
+    @PostMapping("/category")
+    void addCategoria(@RequestBody Categoria categoria) {
+        categoriaRepository.save(categoria);
+    }
+    
+    // Faltan los metodos para editar y borrar una categoria
+
 }
--- portal-template-spring/src/main/java/portal/template/portaltemplatespring/controllers/PerfilController.java
+++ portal-template-spring/src/main/java/portal/template/portaltemplatespring/controllers/PerfilController.java
@@ -2,9 +2,26 @@ package portal.template.portaltemplatespring.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import portal.template.portaltemplatespring.models.dao.PerfilRepository;
+import portal.template.portaltemplatespring.models.entity.Perfil;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 @RestController
 public class PerfilController {
     
+    private PerfilRepository perfilRepository;
 
+    public PerfilController(PerfilRepository _perfilRepository) {
+        this.perfilRepository = _perfilRepository;
+    }
+
+    @PostMapping("/profile")
+    void savePerfil(@RequestBody Perfil perfil) {
+        perfilRepository.save(perfil);
+    }
+    
 
 }
