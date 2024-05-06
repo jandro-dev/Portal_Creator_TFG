7c591c914e7851a8b76763b8e88e1c3505ed9068
afrancop05 <jandro.develop@gmail.com>
2024-05-06 19:13:56 +0200

    Update: Rutas de las vistas configuradas

--- portal-template-angular/src/app/app.routes.ts
+++ portal-template-angular/src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { WebComponent } from './views/web/web.component';
+import { InicioComponent } from './views/inicio/inicio.component';
+import { PerfilComponent } from './views/perfil/perfil.component';
+import { CategoriaComponent } from './views/categoria/categoria.component';
+import { PortalComponent } from './views/portal/portal.component';
+import { NotfoundComponent } from './views/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: InicioComponent,
+  },
+  {
+    path: 'web',
+    component: WebComponent,
+  },
+  {
+    path: 'perfil',
+    component: PerfilComponent,
+  },
+  {
+    path: 'categoria',
+    component: CategoriaComponent,
+  },
+  {
+    path: 'portal',
+    component: PortalComponent,
+  },
+  {
+    path: 'notfound',
+    component: NotfoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'notfound',
+  },
+];
