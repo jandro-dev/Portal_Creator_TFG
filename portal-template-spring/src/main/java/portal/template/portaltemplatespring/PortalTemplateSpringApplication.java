89a0d787a4bc6f905a327549170ee91384472df9
afrancop05 <jandro.develop@gmail.com>
2024-05-06 17:59:00 +0200

    Add: Backend and Frontend

---
+++ portal-template-spring/src/main/java/portal/template/portaltemplatespring/PortalTemplateSpringApplication.java
@@ -0,0 +1,13 @@
+package portal.template.portaltemplatespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalTemplateSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalTemplateSpringApplication.class, args);
+	}
+
+}
---
+++ portal-template-spring/src/test/java/portal/template/portaltemplatespring/PortalTemplateSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package portal.template.portaltemplatespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalTemplateSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
