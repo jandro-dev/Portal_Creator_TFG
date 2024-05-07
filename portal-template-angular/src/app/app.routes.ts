f19717a6a3f5c6071c559e9fe88cab921d7b6c1f
jandro-dev <jandro.develop@gmail.com>
2024-05-07 20:37:40 +0200

    Update: Ruta web eliminada

--- portal-template-angular/src/app/app.routes.ts
+++ portal-template-angular/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { WebComponent } from './views/web/web.component';
 import { InicioComponent } from './views/inicio/inicio.component';
 import { PerfilComponent } from './views/perfil/perfil.component';
 import { CategoriaComponent } from './views/categoria/categoria.component';
@@ -10,10 +9,6 @@ export const routes: Routes = [
   {
     path: '',
     component: InicioComponent,
-  },
-  {
-    path: 'web',
-    component: WebComponent,
   },
   {
     path: 'perfil',
