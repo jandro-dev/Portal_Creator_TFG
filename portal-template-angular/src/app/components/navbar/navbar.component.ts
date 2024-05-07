32049a97f07b346648ea70894a1a2e20501cedda
jandro-dev <jandro.develop@gmail.com>
2024-05-07 20:34:57 +0200

    Update: Navbar para moviles

--- portal-template-angular/src/app/components/navbar/navbar.component.ts
+++ portal-template-angular/src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'navbarComponent',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  isMenuMovil = true;
   @Input() bgColor: string = '#000';
   @Input() color: string = '#fff';
+
+  // Funcion para entrada / salida menu movil
+  alternarMenu() {
+    this.isMenuMovil = !this.isMenuMovil;
+  }
 }
