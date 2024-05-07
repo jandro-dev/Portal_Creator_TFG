87182dcbacf98162ec1527994b1859183d2788ac
jandro-dev <jandro.develop@gmail.com>
2024-05-07 19:17:01 +0200

    Update: Componente card modular hecho y probado

--- portal-template-angular/src/app/components/card/card.component.ts
+++ portal-template-angular/src/app/components/card/card.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-card',
+  selector: 'cardComponent',
   standalone: true,
   imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
+  @Input() urlCard: string = '';
+  @Input() titulo: string = 'Titulo';
+  @Input() descripcion: string = 'Descripcion de la card';
+  @Input() color: string = '#fff';
+  @Input() colorHover: string = '#000';
 
+  isHover: boolean = false;
+
+  entradaHover() {
+    this.isHover = true;
+  }
+
+  salidaHover() {
+    this.isHover = false;
+  }
 }
--- portal-template-angular/src/app/views/inicio/inicio.component.ts
+++ portal-template-angular/src/app/views/inicio/inicio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
