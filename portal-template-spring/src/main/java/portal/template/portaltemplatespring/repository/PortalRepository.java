55e970c879d80b3f5920fadb1e5703bea790703c
afrancop05 <jandro.develop@gmail.com>
2024-05-09 19:04:20 +0200

    Update: Repositorios movidos a carpeta repository y actualizados los ids de la interfaz

--- portal-template-spring/src/main/java/portal/template/portaltemplatespring/repository/PortalRepository.java
+++ portal-template-spring/src/main/java/portal/template/portaltemplatespring/repository/PortalRepository.java
@@ -1,4 +1,4 @@
-package portal.template.portaltemplatespring.models.dao;
+package portal.template.portaltemplatespring.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -6,4 +6,4 @@ import org.springframework.stereotype.Repository;
 import portal.template.portaltemplatespring.models.entity.Portal;
 
 @Repository
-public interface PortalRepository extends CrudRepository<Portal,Long> {}
+public interface PortalRepository extends CrudRepository<Portal,Integer> {}
