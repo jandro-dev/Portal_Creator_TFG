de89e0cb22dfda27c2dcdf666b94e6b2c7678ea1
afrancop05 <jandro.develop@gmail.com>
2024-05-10 22:05:17 +0200

    Update: Vista perfil realizada

--- portal-template-angular/src/app/views/perfil/perfil.component.ts
+++ portal-template-angular/src/app/views/perfil/perfil.component.ts
@@ -1,12 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
+import { PerfilService } from '../../services/perfil.service';
+import { Perfil } from '../../models/perfil.interface';
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './perfil.component.html',
-  styleUrl: './perfil.component.css'
+  styleUrl: './perfil.component.css',
 })
-export default class PerfilComponent {
+export default class PerfilComponent implements OnInit {
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+  private perfilService = inject(PerfilService);
 
+  form?: FormGroup;
+  perfil?: Perfil;
+  errors: string[] = [];
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.perfilService.getPerfil(parseInt(id)).subscribe((perfil) => {
+        this.perfil = perfil;
+        this.form = this.fb.group({
+          nombre: [perfil.nombre, [Validators.required]],
+          apellidos: [perfil.apellidos, [Validators.required]],
+          colorweb1: [
+            perfil.colorWeb1,
+            [
+              Validators.required,
+              Validators.pattern('#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})'),
+            ],
+          ],
+          colorweb2: [
+            perfil.colorWeb2,
+            [
+              Validators.required,
+              Validators.pattern('#[A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})'),
+            ],
+          ],
+        });
+      });
+    } else {
+      this.form = this.fb.group({
+        nombre: ['', [Validators.required]],
+        apellidos: ['', [Validators.required]],
+        colorWeb1: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern('#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})'),
+          ],
+        ],
+        colorWeb2: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern('#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})'),
+          ],
+        ],
+      });
+    }
+  }
+
+  save() {
+    if (this.form?.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const perfilForm = this.form!.value;
+    let request: Observable<Perfil>;
+
+    if (this.perfil) {
+      request = this.perfilService.updatePerfil(this.perfil.id, perfilForm);
+    } else {
+      request = this.perfilService.createPerfil(perfilForm);
+    }
+
+    request.subscribe({
+      next: () => {
+        this.errors = [];
+        this.router.navigate(['/']);
+      },
+      error: (response) => {
+        this.errors = response.error.errors;
+      },
+    });
+  }
 }
