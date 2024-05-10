226190d028788e2e2715bb4526823e0eb2b0e01a
jandro-dev <jandro.develop@gmail.com>
2024-05-10 08:33:02 +0200

    Fix: Correcion @ID en entidades

--- portal-template-spring/src/main/java/portal/template/portaltemplatespring/models/entity/Categoria.java
+++ portal-template-spring/src/main/java/portal/template/portaltemplatespring/models/entity/Categoria.java
@@ -3,12 +3,11 @@ package portal.template.portaltemplatespring.models.entity;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
