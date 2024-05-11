2e30ec036ab18aed56bd741fd1f282ec1ebc6c31
afrancop05 <jandro.develop@gmail.com>
2024-05-11 12:47:00 +0200

    Update: CRUD de categoria terminado

--- portal-template-angular/src/app/views/categoria/categoria.component.ts
+++ portal-template-angular/src/app/views/categoria/categoria.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { CategoriaService } from '../../services/categoria.service';
+import { Categoria } from '../../models/categoria.interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-categoria',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './categoria.component.html',
-  styleUrl: './categoria.component.css'
+  styleUrl: './categoria.component.css',
 })
-export default class CategoriaComponent {
+export default class CategoriaComponent implements OnInit {
+  private fb = inject(FormBuilder);
+  private route = inject(ActivatedRoute);
+  private categoriaService = inject(CategoriaService);
 
+  form?: FormGroup;
+  categoria?: Categoria;
+  errors: string[] = [];
+
+  ngOnInit(): void {
+    const cid = this.route.snapshot.paramMap.get('cid');
+
+    if (cid) {
+      this.categoriaService.getCategoria(parseInt(cid)).subscribe((categoria) => {
+        this.categoria = categoria;
+        this.form = this.fb.group({
+          nombre: [categoria.nombre, [Validators.required]]
+        });
+      });
+    } else {
+      this.form = this.fb.group({
+        nombre: ['', [Validators.required]]
+      });
+    }
+  }
+
+  save() {
+    if (this.form?.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const categoriaForm = this.form!.value;
+    let request: Observable<Categoria>;
+
+    if (this.categoria) {
+      request = this.categoriaService.updateCategoria(this.categoria.id, categoriaForm);
+    } else {
+      request = this.categoriaService.createCategoria(categoriaForm);
+    }
+
+    request.subscribe({
+      next: () => {
+        this.errors = [];
+        window.location.href = '/';
+      },
+      error: (response) => {
+        this.errors = response.error.errors;
+      },
+    });
+  }
 }
--- portal-template-angular/src/app/views/inicio/inicio.component.ts
+++ portal-template-angular/src/app/views/inicio/inicio.component.ts
@@ -3,6 +3,8 @@ import { CardComponent } from '../../components/card/card.component';
 import { PerfilService } from '../../services/perfil.service';
 import { Perfil } from '../../models/perfil.interface';
 import { RouterModule } from '@angular/router';
+import { Categoria } from '../../models/categoria.interface';
+import { CategoriaService } from '../../services/categoria.service';
 
 @Component({
   selector: 'app-inicio',
@@ -13,6 +15,8 @@ import { RouterModule } from '@angular/router';
 })
 export default class InicioComponent {
   private perfilService = inject(PerfilService);
+  private categoriaService = inject(CategoriaService);
+  categorias: Categoria[] = [];
 
   perfil?: Perfil;
   color: string = '';
@@ -23,6 +27,19 @@ export default class InicioComponent {
       this.perfil = perfil;
       this.color = perfil.colorWeb2;
       this.bgColor = perfil.colorWeb1;
+    });
+    this.loadCategorias();
+  }
+
+  loadCategorias() {
+    this.categoriaService.listCategorias().subscribe((categorias) => {
+      this.categorias = categorias;
+    });
+  }
+
+  deleteCategoria(categoria: Categoria) {
+    this.categoriaService.deleteCategoria(categoria.id).subscribe(() => {
+      this.loadCategorias();
     });
   }
 }
