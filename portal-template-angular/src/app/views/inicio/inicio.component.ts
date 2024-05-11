7445816ae30ba2ae2d71a56dfe94436ad2bec0d2
afrancop05 <jandro.develop@gmail.com>
2024-05-11 12:03:06 +0200

    Update: Vista segun perfil antes de añadir categoria

--- portal-template-angular/src/app/views/inicio/inicio.component.ts
+++ portal-template-angular/src/app/views/inicio/inicio.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { PerfilService } from '../../services/perfil.service';
+import { Perfil } from '../../models/perfil.interface';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterModule],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrl: './inicio.component.css',
 })
 export default class InicioComponent {
+  private perfilService = inject(PerfilService);
 
+  perfil?: Perfil;
+  color: string = '';
+  bgColor: string = '';
+
+  ngOnInit(): void {
+    this.perfilService.getPerfil(1).subscribe((perfil) => {
+      this.perfil = perfil;
+      this.color = perfil.colorWeb2;
+      this.bgColor = perfil.colorWeb1;
+    });
+  }
 }
