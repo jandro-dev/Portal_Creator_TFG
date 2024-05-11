217d36cb18e58b696c07633a15dea35a38c09ec5
afrancop05 <jandro.develop@gmail.com>
2024-05-11 16:57:59 +0200

    Update: Imports entidad Portal

--- portal-template-spring/src/main/java/portal/template/portaltemplatespring/models/entity/Portal.java
+++ portal-template-spring/src/main/java/portal/template/portaltemplatespring/models/entity/Portal.java
@@ -1,7 +1,5 @@
 package portal.template.portaltemplatespring.models.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
