35ad394ea957bb2a370c701bb32e7bac090b2d15
afrancop05 <jandro.develop@gmail.com>
2026-08-31 10:52:54 +0200

    Update borrar perfil con categorias

--- portal-template-spring/src/main/java/portal/template/portaltemplatespring/models/entity/Perfil.java
+++ portal-template-spring/src/main/java/portal/template/portaltemplatespring/models/entity/Perfil.java
@@ -1,9 +1,16 @@
 package portal.template.portaltemplatespring.models.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -36,4 +43,7 @@ public class Perfil {
     @NonNull
     private String colorWeb2;
 
+    @OneToMany(mappedBy = "perfil",cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Categoria> categorias = new ArrayList<>();
 }
